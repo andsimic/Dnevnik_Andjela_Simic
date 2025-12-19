c43e34a5e70cbb259543893ec00c880f4919e77e
andsimic <andsimic0246@gmail.com>
2025-12-19 20:14:03 +0100

    Update Osoba.cs

--- Dnevnik_Andjela_Simic/Osoba.cs
+++ Dnevnik_Andjela_Simic/Osoba.cs
@@ -27,7 +27,7 @@ namespace Dnevnik_Andjela_Simic
             tabela = new DataTable();
             da.Fill(tabela);
         }
-        private void Txt_Load()
+        private void prikazi()
         {
             if (tabela.Rows.Count == -1)
             {
@@ -51,16 +51,6 @@ namespace Dnevnik_Andjela_Simic
                 txt_pass.Text = tabela.Rows[br_sloga]["pass"].ToString();
                 txt_uloga.Text = tabela.Rows[br_sloga]["uloga"].ToString();
             }
-            if(br_sloga == 0)
-            {
-                button6.Enabled = false;
-                button7.Enabled = false;
-            }
-            else
-            {
-                button6.Enabled = true;
-                button7.Enabled = true;
-            }
             if (br_sloga == tabela.Rows.Count - 1)
             {
                 button1.Enabled = false;
@@ -71,40 +61,121 @@ namespace Dnevnik_Andjela_Simic
                 button1.Enabled = true;
                 button2.Enabled = true;
             }
+
+            if (br_sloga == 0)
+            {
+                button6.Enabled = false;
+                button7.Enabled = false;
+            }
+            else
+            {
+                button6.Enabled = true;
+                button7.Enabled = true;
+            }
+
         }
         private void Osoba_Load(object sender, EventArgs e)
         {
             Load_Data();
-            Txt_Load();
+            prikazi();
         }
 
         private void buttonX_Click(object sender, EventArgs e)
         {
-            Application.Exit();
+            this.Hide();
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
             br_sloga--;
-            Txt_Load();
+            prikazi();
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
             br_sloga = 0;
-            Txt_Load();
+            prikazi();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             br_sloga++;
-            Txt_Load();
+            prikazi();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             br_sloga = tabela.Rows.Count - 1;
-            Txt_Load();
+            prikazi();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string naredba = "INSERT INTO osoba VALUES('";
+            naredba = naredba + txt_ime.Text + "','";
+            naredba = naredba + txt_prezime.Text + "','";
+            naredba = naredba + txt_adresa.Text + "','";
+            naredba = naredba + txt_jmbg.Text + "','";
+            naredba = naredba + txt_email.Text + "','";
+            naredba = naredba + txt_pass.Text + "',')";
+            naredba = naredba + txt_uloga.Text + "',1)";
+            SqlConnection veza = konekcija.povezi();
+            SqlCommand komanda = new SqlCommand(naredba, veza);
+            veza.Open();
+            komanda.ExecuteNonQuery();
+            veza.Close();
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM osoba", veza);
+            tabela = new DataTable();
+            da.Fill(tabela);
+            br_sloga = tabela.Rows.Count - 1;
+            prikazi();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string naredba = "UPDATE osoba SET ";
+            naredba += "ime='" + txt_ime.Text + "',";
+            naredba += "prezime='" + txt_prezime.Text + "',";
+            naredba += "adresa='" + txt_adresa.Text + "',";
+            naredba += "jmbg='" + txt_jmbg.Text + "',";
+            naredba += "email='" + txt_email.Text + "',";
+            naredba += "pass='" + txt_pass.Text + "',";
+            naredba += "uloga='" + txt_uloga.Text + "'";
+            naredba += "WHERE id = " + txt_id.Text;
+            SqlConnection veza = konekcija.povezi();
+            SqlCommand komanda = new SqlCommand(naredba, veza);
+            veza.Open();
+            komanda.ExecuteNonQuery();
+            veza.Close();
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM osoba", veza);
+            tabela = new DataTable();
+            da.Fill(tabela);
+            prikazi();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (br_sloga == tabela.Rows.Count - 1)
+            {
+                br_sloga--;
+            }
+            string naredba = "DELETE FROM osoba WHERE id=" + txt_id.Text;
+            SqlConnection veza = konekcija.povezi();
+            SqlCommand komanda = new SqlCommand(naredba, veza);
+            try
+            {
+                veza.Open();
+                komanda.ExecuteNonQuery();
+                veza.Close();
+            }
+            catch (Exception greska)
+            {
+                MessageBox.Show(greska.GetType().ToString());
+            }
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM osoba", veza);
+            tabela = new DataTable();
+            da.Fill(tabela);
+            prikazi();
         }
     }
 }
